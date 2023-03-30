0541f2add927899c6e60e2416d21c2e158a0e68a
kos3126 <60677527+kos3126@users.noreply.github.com>
2023-03-30 18:32:51 +0900

    build: add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
-});
+  integrations: [tailwind(), react(), preact()],
+})
---
+++ src/components/Card.tsx
@@ -0,0 +1,24 @@
+/** @jsxImportSource preact */
+
+import { useState } from 'preact/hooks'
+
+export default function Card() {
+  const [test, setTest] = useState('foo')
+
+  return (
+    <li className="flex items-center justify-between gap-x-4 rounded-lg bg-indigo-800 p-6 text-white">
+      <h2>Test</h2>
+      <span>&rarr;</span>
+      <p>{test}</p>
+      <button
+        type="button"
+        onClick={() => {
+          setTest((prev) => (prev === 'foo' ? 'bar' : 'foo'))
+        }}
+        className="rounded-lg border-2 border-white p-2"
+      >
+        Button
+      </button>
+    </li>
+  )
+}
