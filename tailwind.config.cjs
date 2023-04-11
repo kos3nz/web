e097e09818c7d21578257f3afa49b2eda0ff4495
kos3126 <60677527+kos3126@users.noreply.github.com>
2023-04-11 17:14:17 +0900

    chore: update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          // 'Helvetica Neue', // macOS English
+          // 'Arial', // window English
+          // 'Hiragino Sans', // macOS Japanese
+          // 'Hiragino Kaku Gothic ProN', // macOS Japanese
+          // 'BIZ UDPGothic', // window Japanese
+          // 'Meiryo', // window Japanese
+          ...defaultTheme.fontFamily.sans // sans-serif for android
+        ],
+      },
+    },
   },
   plugins: [],
 }
