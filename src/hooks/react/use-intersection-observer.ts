afde65ba9a8b574393b2476c94f64a7600df0603
kos3126 <60677527+kos3126@users.noreply.github.com>
2023-04-20 19:03:44 +0900

    feat: add other useful hooks

---
+++ src/hooks/react/use-intersection-observer.ts
@@ -0,0 +1,61 @@
+/** @jsxImportSource react */
+
+import type { RefObject } from 'react'
+import { useEffect, useRef, useState } from 'react'
+
+type IntersectionObserverOptions = IntersectionObserverInit & {
+  once?: boolean
+}
+
+const defaultOptions: IntersectionObserverOptions = {
+  root: null, // Viewport is root element when the value is null,
+  rootMargin: '0px 0px 0px 0px', // Viewport without margin
+  threshold: 0, // Observer callback will be executed when root-margin hits the top of the element
+  once: false,
+}
+
+export function useIntersectionObserver<TElement>(
+  options?: IntersectionObserverOptions,
+  callback?: () => void,
+): [RefObject<TElement>, boolean] {
+  const [entry, setEntry] = useState<IntersectionObserverEntry>()
+  const [isIntersecting, setIsIntersecting] = useState(false)
+  const containerRef = useRef<TElement>(null)
+  const frozen = isIntersecting && options?.once
+
+  useEffect(() => {
+    const hasSupport = !!window.IntersectionObserver
+
+    if (!hasSupport) return
+
+    const { root, rootMargin, threshold } = { ...defaultOptions, ...options }
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setEntry(entry)
+      },
+      { root, rootMargin, threshold } as IntersectionObserverInit,
+    )
+
+    observer.observe(containerRef.current as Element)
+
+    return () => {
+      observer.disconnect()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    if (!entry || frozen) return
+
+    setIsIntersecting(entry.isIntersecting)
+
+    if (entry.isIntersecting && callback) callback()
+
+    return () => {
+      setEntry(undefined)
+    }
+  }, [entry, frozen, callback])
+
+  return [containerRef, isIntersecting]
+}
---
+++ src/hooks/solidjs/use-effect.ts
@@ -0,0 +1,25 @@
+import { createEffect, onCleanup } from 'solid-js'
+// import { isDefined, isFunction } from '../utils';
+
+export function useEffect(callback: () => unknown): void {
+  createEffect(() => {
+    if (isDefined(callback) && isFunction(callback)) {
+      const cleanup = callback()
+      if (isFunction(cleanup)) {
+        onCleanup(() => {
+          cleanup()
+        })
+      }
+    }
+  })
+}
+
+export function isDefined<T>(value: T): value is T {
+  return typeof value !== 'undefined' && value !== null
+}
+
+export function isFunction(value: unknown): value is (...args: any[]) => any {
+  return typeof value === 'function'
+}
+
+export const isBrowser = typeof window !== 'undefined'
