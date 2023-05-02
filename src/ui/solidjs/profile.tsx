df0b2a8be19095a78cd6c284440d220c57dfd976
kos3126 <60677527+kos3126@users.noreply.github.com>
2023-05-02 16:52:53 +0900

    feat: update icons and profile component

--- src/ui/solidjs/profile.tsx
+++ src/ui/solidjs/profile.tsx
@@ -1,14 +1,15 @@
 /** @jsxImportSource solid-js */
 
-import MotionLogo from '@ui/solidjs/motion-logo'
+import MotionLogo from 'ui/solidjs/motion-logo'
 
 export default function Profile() {
   return (
-    <div class="flex flex-col items-center gap-6 rounded-xl border border-slate-600 px-10 py-8 md:flex-row md:gap-8">
-      <div class="box flex h-24 w-24 shrink-0 items-center justify-center rounded-full border border-slate-500">
+    <div class="flex flex-col items-center gap-6 rounded-xl border border-slate-600/50 px-10 py-8 md:flex-row md:gap-8">
+      <div class="box flex h-24 w-24 shrink-0 items-center justify-center rounded-full border border-slate-600/50">
         <MotionLogo width={20} height={45} />
       </div>
-      <div class="h-px w-full bg-slate-600 md:h-full md:w-px" />
+
+      <div class="h-px w-full bg-slate-600/50 md:h-full md:w-px" />
 
       <div class="flex max-w-lg items-center">
         元ビール醸造士。独学でプログラミング学び、現在はWebエンジニアとして活動しています。好きな技術はReact,
