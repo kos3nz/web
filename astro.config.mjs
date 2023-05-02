ee542afb9040f7ee3d6c40b5f45dc628d0779968
kos3126 <60677527+kos3126@users.noreply.github.com>
2023-05-02 17:09:32 +0900

    feat: add syntax highlighting for markdown/mdx code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,74 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-import preact from '@astrojs/preact'
 import solidJs from '@astrojs/solid-js'
+import mdx from '@astrojs/mdx'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+// import rehypePresetMinify from 'rehype-preset-minify'
+import rehypePrettyCode from 'rehype-pretty-code'
+import moonLightII from './theme/moonlight-ii.json'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), preact(), solidJs()],
+  markdown: {
+    // Disable built-in syntax highlighting (This needs to be false if you are using rehype-pretty-code)
+    syntaxHighlight: false,
+    rehypePlugins: [
+      // Headingsにid attributeを追加
+      rehypeSlug,
+      [
+        // Headingsにリンクを付与
+        rehypeAutolinkHeadings,
+        {
+          // `prepend`・`append`・`wrap`・`before`・`after` でリンクの挿入位置を指定
+          behavior: 'prepend',
+          // `a` 要素に付与する属性
+          properties: {
+            className: ['anchor'],
+          },
+          // `a` 要素の子要素を定義する
+          content: {
+            type: 'element',
+            tagName: 'span',
+            properties: {
+              className: ['anchor-icon'],
+            },
+            children: [
+              {
+                type: 'text',
+                value: '#',
+              },
+            ],
+          },
+        },
+      ],
+      [
+        rehypePrettyCode,
+        {
+          theme: moonLightII,
+          // Keep the background or use a custom background color?
+          keepBackground: true,
+
+          // Callback hooks to add custom logic to nodes when visiting them.
+          onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: 'text', value: ' ' }]
+            }
+          },
+          onVisitHighlightedLine(node) {
+            // Each line node by default has `class="line"`.
+            node.properties.className.push('highlighted')
+          },
+          onVisitHighlightedWord(node) {
+            // Each word node has no className by default.
+            node.properties.className = ['word']
+          },
+        },
+      ],
+      // rehypePresetMinify,
+    ],
+  },
+  integrations: [tailwind(), react(), solidJs(), mdx()],
 })
---
+++ build-theme.cjs
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const colors = require('./theme/colors.cjs')
+
+const file = 'moonlight-ii.json'
+
+let text = fs.readFileSync(`./theme/${file}`, 'utf8')
+
+Object.entries(colors).forEach(([colorVar, hexStr]) => {
+  text = text.replace(new RegExp(`\\${colorVar}`, 'g'), hexStr)
+})
+
+fs.writeFileSync(`./theme/${file}`, text)
