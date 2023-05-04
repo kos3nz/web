e70fe8594f300a40f06d478b0a8e16a355e3c805
kos3126 <60677527+kos3126@users.noreply.github.com>
2023-05-04 22:19:21 +0900

    refactor: create filterValue function for filter state

--- src/ui/solidjs/posts.tsx
+++ src/ui/solidjs/posts.tsx
@@ -1,24 +1,12 @@
 /** @jsxImportSource solid-js */
 
-import { useStore } from '@nanostores/solid'
 import { For } from 'solid-js'
-import { searchQuery, selectedTags } from 'store/filterStore'
+import { filterValue } from 'store/filterStore'
 import type { Post } from 'types/global'
 import PostCard from 'ui/solidjs/post-card.tsx'
 
 export default function Posts(props: { posts: Post[] }) {
-  const $searchQuery = useStore(searchQuery)
-  const $selectedTags = useStore(selectedTags)
-
-  const filtered = () =>
-    props.posts.filter((post) => {
-      if (Object.values($selectedTags()).length > 0) {
-        // if any tag is not found in selected tags, return false (remove from the posts)
-        if (!post.tags.some((tag) => !!$selectedTags()[tag])) return false
-      }
-
-      return post.title.toLowerCase().includes($searchQuery().toLowerCase())
-    })
+  const filtered = () => filterValue(props.posts)
 
   return (
     <div class="mt-12 grid gap-6 sm:grid-cols-2 md:gap-8 lg:grid-cols-3">
