635c43a7097adfc92e1d2da70959e53e6f0b6d10
kos3126 <60677527+kos3126@users.noreply.github.com>
2023-05-05 15:31:37 +0900

    refactor: change styles for playground

--- src/ui/solidjs/playground.tsx
+++ src/ui/solidjs/playground.tsx
@@ -9,7 +9,7 @@ export default function Playground(props: {
   return (
     // eslint-disable-next-line tailwindcss/no-contradicting-classname
     <div class="relative overflow-hidden rounded-xl border border-slate-400/20 bg-slate-950 bg-[image:radial-gradient(circle_at_1px_1px,_#0d182a_1px,_transparent_0)] bg-[size:16px_16px] bg-[position:5px_5px]">
-      <div class="absolute inset-0 bg-gradient-to-tr from-indigo-500/10 to-cyan-500/10" />
+      <div class="absolute inset-0 bg-gradient-to-tr from-indigo-700/20 to-cyan-700/20" />
 
       <div class={'relative ' + props.class}>{props.children}</div>
     </div>
