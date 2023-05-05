51877371819eb02ace987a928890fa45665e0bee
kos3126 <60677527+kos3126@users.noreply.github.com>
2023-05-05 22:27:37 +0900

    refactor: update form validation with zod

--- src/ui/react/form.tsx
+++ src/ui/react/form.tsx
@@ -1,40 +1,43 @@
 /** @jsxImportSource react */
 
+import { zodResolver } from '@hookform/resolvers/zod'
 import {
-  useForm,
+  FieldErrors,
+  RegisterOptions,
   SubmitHandler,
   UseFormRegister,
-  RegisterOptions,
-  FieldErrors,
+  useForm,
 } from 'react-hook-form'
+import { z } from 'zod'
 
-type ValidationInputs = {
-  name: string
-  companyName: string
-  email: string
-  message: string
-}
+const schema = z.object({
+  name: z
+    .string()
+    .min(1, { message: 'お名前を入力してください' })
+    .max(30, { message: '30文字以内で入力してください' }),
+  companyName: z
+    .string()
+    .min(1, { message: '会社名を入力してください' })
+    .max(30, { message: '30文字以内で入力してください' }),
+  email: z
+    .string()
+    .min(1, { message: 'メールアドレスを入力してください' })
+    .email({ message: '有効なメールアドレスを記入してください' }),
+  message: z.string(),
+})
 
-type InputProps = {
-  name: keyof ValidationInputs
-  label?: string
-  register: UseFormRegister<ValidationInputs>
-  options?: RegisterOptions
-  placeholder?: string
-  errors?: FieldErrors<ValidationInputs>
-  errorMessage?: string
-}
-
-const emailRegex = new RegExp(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)
+type Schema = z.infer<typeof schema>
 
 export default function ValidatedForm() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<ValidationInputs>()
+  } = useForm<Schema>({
+    resolver: zodResolver(schema),
+  })
 
-  const onSubmit: SubmitHandler<ValidationInputs> = (data) => console.log(data)
+  const onSubmit: SubmitHandler<Schema> = (data) => console.log(data)
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -42,44 +45,47 @@ export default function ValidatedForm() {
         <FormInput
           name="name"
           register={register}
-          options={{ maxLength: 30, required: true }}
           placeholder="Name / お名前"
           errors={errors}
-          errorMessage="お名前を入力してください(30文字以内)"
         />
         <FormInput
           name="companyName"
           register={register}
-          options={{ maxLength: 50, required: true }}
           placeholder="Company Name / 会社名"
           errors={errors}
-          errorMessage="会社名を入力してください(30文字以内)"
         />
         <FormInput
           name="email"
           register={register}
-          options={{ pattern: emailRegex, required: true }}
           placeholder="Email / メールアドレス"
           errors={errors}
-          errorMessage="有効なメールアドレスを入力してください"
         />
 
         <textarea
           {...register('message')}
-          className="textarea w-full bg-slate-800 focus-visible:outline-indigo-500"
+          className="textarea w-full bg-slate-800 text-base focus-visible:outline-slate-200"
           rows={10}
           placeholder="Message / お問い合わせ内容"
         />
 
         <button
           type="submit"
-          className="btn mx-auto block bg-indigo-600 hover:bg-indigo-600/60 focus-visible:outline-indigo-500"
+          className="btn mx-auto block bg-indigo-600 hover:bg-indigo-600/60 focus-visible:outline-slate-200"
         >
           Confirm / 確認する
         </button>
       </div>
     </form>
   )
+}
+
+type InputProps = {
+  name: keyof Schema
+  label?: string
+  register: UseFormRegister<Schema>
+  options?: RegisterOptions
+  placeholder?: string
+  errors?: FieldErrors<Schema>
 }
 
 function FormInput({
@@ -89,14 +95,13 @@ function FormInput({
   options,
   placeholder,
   errors,
-  errorMessage,
 }: InputProps) {
   return (
     <div className="form-control space-y-2">
       {/* <label className='pointer-events-none'>{label}</label> */}
       <input
         {...register(name, { ...options })}
-        className="input w-full bg-slate-800 focus-visible:outline-indigo-500"
+        className="input w-full bg-slate-800 focus-visible:outline-slate-200"
         placeholder={placeholder}
         aria-invalid={errors?.[name] ? 'true' : 'false'}
       />
@@ -116,7 +121,7 @@ function FormInput({
               d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
             />
           </svg>
-          <span>{errorMessage}</span>
+          <span>{errors[name]?.message}</span>
         </p>
       )}
     </div>
