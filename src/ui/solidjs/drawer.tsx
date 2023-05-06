05ed6e8a6d37265e10224fd583d55821badc9fc4
kos3126 <60677527+kos3126@users.noreply.github.com>
2023-05-06 20:54:25 +0900

    refactor: re-design header and drawer components

--- src/ui/solidjs/drawer.tsx
+++ src/ui/solidjs/drawer.tsx
@@ -2,37 +2,35 @@
 
 import { useStore } from '@nanostores/solid'
 import cx from 'clsx'
-import { isDrawerOpen } from 'store/drawerStore'
+import { isDrawerOpen } from 'store/drawer-store.ts'
 
 export default function Drawer() {
   const $isDrawerOpen = useStore(isDrawerOpen)
 
   return (
-    <>
-      <button
-        id="drawer-handler"
-        class="space-y-[4px] p-1"
-        onClick={() => isDrawerOpen.set(!$isDrawerOpen())}
-      >
-        <span
-          class={cx(
-            'block h-[2px] w-5 rounded-md bg-current duration-300',
-            $isDrawerOpen() ? 'translate-y-1.5 -rotate-[135deg]' : '',
-          )}
-        />
-        <span
-          class={cx(
-            'block h-[2px] w-3.5 rounded-md bg-current',
-            $isDrawerOpen() ? 'opacity-0 duration-100' : 'duration-700',
-          )}
-        />
-        <span
-          class={cx(
-            'block h-[2px] rounded-md bg-current duration-300',
-            $isDrawerOpen() ? 'w-5 -translate-y-1.5 rotate-[135deg]' : 'w-2',
-          )}
-        />
-      </button>
-    </>
+    <button
+      id="drawer-trigger"
+      class="space-y-[4px] p-1"
+      onClick={() => isDrawerOpen.set(!$isDrawerOpen())}
+    >
+      <span
+        class={cx(
+          'block h-[2px] w-5 rounded-md bg-current duration-300',
+          $isDrawerOpen() ? 'translate-y-1.5 -rotate-[135deg]' : '',
+        )}
+      />
+      <span
+        class={cx(
+          'block h-[2px] w-3.5 rounded-md bg-current',
+          $isDrawerOpen() ? 'opacity-0 duration-100' : 'duration-700',
+        )}
+      />
+      <span
+        class={cx(
+          'block h-[2px] rounded-md bg-current duration-300',
+          $isDrawerOpen() ? 'w-5 -translate-y-1.5 rotate-[135deg]' : 'w-2',
+        )}
+      />
+    </button>
   )
 }
--- src/ui/solidjs/header.tsx
+++ src/ui/solidjs/header.tsx
@@ -9,17 +9,15 @@ export default function Header(props: { children: JSXElement }) {
   const direction = createScrollDirection(bounds)
 
   return (
-    <div
+    <header
       class={cx(
-        'fixed left-1/2 top-3 z-50 w-full max-w-6xl translate-x-[-50%] px-6 transition-transform duration-700 ease-in-out',
+        'flex w-full items-center justify-between gap-x-6 rounded-xl border border-slate-400/20 bg-slate-900 px-4 py-2 backdrop-blur-sm transition-transform duration-500 ease-in-out md:px-6 md:py-2 [@supports(backdrop-filter:blur(0px))]:bg-slate-900/80',
         direction() === 'down'
-          ? '-translate-y-[calc(100%+24px)]'
+          ? '-translate-y-[calc(100%+12px)]'
           : 'translate-y-0',
       )}
     >
-      <header class="flex w-full items-center justify-between gap-x-6 rounded-xl border border-slate-600/50 bg-slate-800/90 px-4 py-2 backdrop-blur-sm md:px-6 md:py-2 [@supports(backdrop-filter:blur(0px))]:bg-slate-900/80">
-        {props.children}
-      </header>
-    </div>
+      {props.children}
+    </header>
   )
 }
