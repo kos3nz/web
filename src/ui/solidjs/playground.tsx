4896d9c163ee1f30864badc74053eddeff57b7e9
kos3nz <60677527+kos3nz@users.noreply.github.com>
2023-05-31 13:30:41 +0900

    change styles for mdx code block and playground component

--- src/ui/solidjs/playground.tsx
+++ src/ui/solidjs/playground.tsx
@@ -12,7 +12,7 @@ export default function Playground(props: {
   return (
     <div
       class={cn(
-        'bg-slate-950 bg-[image:radial-gradient(circle_at_1px_1px,_#1e283b_1px,_transparent_0)] bg-[size:20px_20px] bg-[position:8px_8px]',
+        'bg-[image:radial-gradient(circle_at_1px_1px,_#1e283b_1px,_transparent_0)] bg-[size:20px_20px] bg-[position:8px_8px]',
         'relative rounded-xl border border-slate-400/20',
         'overflow-hidden',
         'aspect-[1/1] sm:aspect-[4/3] md:aspect-[10/6]',
