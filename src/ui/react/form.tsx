f823fd71cc90a0c94151352c46ab627be7337a80
kos3nz <60677527+kos3nz@users.noreply.github.com>
2023-06-01 13:46:43 +0900

    install react-hook-form devtools

--- src/ui/react/form.tsx
+++ src/ui/react/form.tsx
@@ -15,6 +15,7 @@ import type { ComponentPropsWithAs } from 'types/helpers.ts'
 import type { ContactSchema } from 'types/validation.types.ts'
 import { contactSchema } from 'utils/form-validation.ts'
 import { RightChevronIcon } from 'ui/react/icons.tsx'
+import { DevTool } from '@hookform/devtools'
 
 export default function Form() {
   const $step = useStore(step)
@@ -24,6 +25,7 @@ export default function Form() {
     register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm<ContactSchema>({
     defaultValues: $contactInfo || undefined,
     resolver: zodResolver(contactSchema),
@@ -37,64 +39,70 @@ export default function Form() {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      aria-hidden={$step !== 1}
-      className="w-full"
-    >
-      <div className="space-y-6 md:space-y-8">
-        <div className="space-y-5">
-          <div className="flex flex-col gap-x-4 gap-y-5 md:flex-row md:items-start">
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        aria-hidden={$step !== 1}
+        className="w-full"
+      >
+        <div className="space-y-6 md:space-y-8">
+          <div className="space-y-5">
+            <div className="flex flex-col gap-x-4 gap-y-5 md:flex-row md:items-start">
+              <FormInput
+                label="お名前"
+                name="name"
+                register={register}
+                placeholder="例: 山田太郎"
+                errors={errors}
+                required
+              />
+              <FormInput
+                label="会社名"
+                name="companyName"
+                register={register}
+                placeholder="例: 株式会社〇〇"
+                errors={errors}
+              />
+            </div>
             <FormInput
-              label="お名前"
-              name="name"
+              label="メールアドレス"
+              name="email"
               register={register}
-              placeholder="例: 山田太郎"
+              placeholder="例: example@email.com"
               errors={errors}
               required
             />
             <FormInput
-              label="会社名"
-              name="companyName"
+              label="電話番号"
+              name="phoneNumber"
               register={register}
-              placeholder="例: 株式会社〇〇"
+              placeholder="例: 090-1234-5678"
               errors={errors}
             />
+            <FormInput
+              as="textarea"
+              label="お問い合わせ内容"
+              name="message"
+              register={register}
+              placeholder="お問い合わせ内容を入力してください"
+              rows={8}
+            />
           </div>
-          <FormInput
-            label="メールアドレス"
-            name="email"
-            register={register}
-            placeholder="例: example@email.com"
-            errors={errors}
-            required
-          />
-          <FormInput
-            label="電話番号"
-            name="phoneNumber"
-            register={register}
-            placeholder="例: 090-1234-5678"
-            errors={errors}
-          />
-          <FormInput
-            as="textarea"
-            label="お問い合わせ内容"
-            name="message"
-            register={register}
-            placeholder="お問い合わせ内容を入力してください"
-            rows={8}
-          />
-        </div>
 
-        <button
-          type="submit"
-          className="btn mx-auto flex items-center gap-x-1 border-0 bg-cyan-400 text-slate-900 hover:bg-cyan-300 focus-visible:outline-cyan-500/75"
-        >
-          Continue
-          <RightChevronIcon />
-        </button>
-      </div>
-    </form>
+          <button
+            type="submit"
+            className="btn mx-auto flex items-center gap-x-1 border-0 bg-cyan-400 text-slate-900 hover:bg-cyan-300 focus-visible:outline-cyan-500/75"
+          >
+            Continue
+            <RightChevronIcon />
+          </button>
+        </div>
+      </form>
+
+      {process.env.NODE_ENV === 'development' && (
+        <DevTool control={control} placement="bottom-right" />
+      )}
+    </>
   )
 }
 
